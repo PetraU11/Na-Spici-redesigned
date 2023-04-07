e15fc617df5c4364ae4eb7d0b2b5c9ceda8a96d5
Petra Ulicna <petra.ulicna@hotmail.com>
2023-04-07 22:20:27 +0200

    js added - carte

---
+++ script.js
@@ -0,0 +1,14 @@
+const drink = document.querySelector("#carte__drink")
+const drinkDiv = document.querySelector(".carte__buttons-drink")
+
+drinkDiv.addEventListener("click", () => {
+ drink.style.display = "inline-block"
+})
+
+
+const food = document.querySelector("#carte__food")
+const foodDiv = document.querySelector(".carte__buttons-food")
+
+foodDiv.addEventListener("click", () => {
+    food.style.display = "inline-block"
+   })
