924a0deb47cd2b6522161200043d1f8ec6d6d04f
Petra Ulicna <petra.ulicna@hotmail.com>
2023-06-03 22:59:04 +0200

    js active buttons test catering and booking

--- scriptBooking.js
+++ scriptBooking.js
@@ -2,7 +2,8 @@
 const button = document.querySelector("#booking-button")
 button.addEventListener("click", () => {
     button.textContent = "Děkujeme! Vaše rezervace byla úspěšně odeslána."
-    
-
 }
 )
+
+const menuButton = document.querySelector("#menu__itemActive")
+menuButton.classList.add(".menu__item.active");
---
+++ scriptCatering.js
@@ -0,0 +1,2 @@
+const menuButton = document.querySelector("#menu__itemActive")
+menuButton.classList.add(".menu__item.active");
