cbf5859b783074ab1ee831ef409d637f8ab738d5
Petra Ulicna <petra.ulicna@hotmail.com>
2023-06-07 22:35:33 +0200

    js bc color for menu buttons added - booking and catering - test

--- scriptBooking.js
+++ scriptBooking.js
@@ -7,3 +7,6 @@ button.addEventListener("click", () => {
 
 const menuButton = document.querySelector("#menu__itemActive")
 menuButton.classList.add(".menu__item.active");
+
+const menuButtonLink = document.querySelector("#menu__linkActive")
+menuButtonLink.classList.add(".menu__link.active");
--- scriptCatering.js
+++ scriptCatering.js
@@ -1,2 +1,5 @@
 const menuButton = document.querySelector("#menu__itemActive")
 menuButton.classList.add(".menu__item.active");
+
+const menuButtonLink = document.querySelector("#menu__linkActive")
+menuButtonLink.classList.add(".menu__link.active");
