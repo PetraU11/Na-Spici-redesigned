359423e5f852d7d0550991bc2b698a74fa2e3eb7
Petra Ulicna <petra.ulicna@hotmail.com>
2023-09-20 21:49:52 +0200

    Menu js modification

--- js/scriptMenu.js
+++ js/scriptMenu.js
@@ -2,21 +2,27 @@ const drink = document.querySelector("#carte__drink")
 const drinkDiv = document.querySelector(".carte__buttons-drink")
 
 drinkDiv.addEventListener("click", () => {
- drink.style.display = "inline-block"
+    if (drink.style.display === "inline-block"){
+ drink.style.display = "none"}
+    else{ 
+    drink.style.display = "inline-block"
+ }
 })
-
 
 
 const food = document.querySelector("#carte__food")
 const foodDiv = document.querySelector(".carte__buttons-food")
 
 foodDiv.addEventListener("click", () => {
-    food.style.display = "inline-block"
+    if (food.style.display === "inline-block"){
+    food.style.display = "none"}
+    else {
+        food.style.display = "inline-block"
+    }
    })
 
 
-const menuButton = document.querySelector("#menu__itemActive")
-menuButton.classList.add(".menu__item.active");
-   
-const menuButtonLink = document.querySelector("#menu__linkActive")
-menuButtonLink.classList.add(".menu__link.active");
+
+
+
+
