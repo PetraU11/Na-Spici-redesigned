322f61d4e974ace9f6ca25b4f54537c33471fb96
Petra Ulicna <petra.ulicna@hotmail.com>
2023-09-22 22:25:09 +0200

    catering section added, to be modified

--- js/scriptCatering.js
+++ js/scriptCatering.js
@@ -3,3 +3,21 @@ menuButton.classList.add(".menu__item.active");
 
 const menuButtonLink = document.querySelector("#menu__linkActive")
 menuButtonLink.classList.add(".menu__link.active");
+
+
+const showButton = document.getElementById("showList")
+const hideButton = document.getElementById("hideList")
+const listPartTwo = document.getElementById("listSecondPart")
+
+showButton.addEventListener("click", () => {
+listPartTwo.style.display = "inline-block"
+showButton.style.display = "none"
+hideButton.style.display = "inline-block"
+})
+
+hideButton.addEventListener("click", () => {
+    listPartTwo.style.display = "none"
+    hideButton.style.display = "none"
+    showButton.style.display = "inline-block"
+
+})
