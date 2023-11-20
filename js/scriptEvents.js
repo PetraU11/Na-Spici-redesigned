d141c64a83c25ae3c3ddbc380f053f1c09deec64
Petra Ulicna <petra.ulicna@hotmail.com>
2023-11-20 22:45:06 +0100

    event leaflets gallery test js

--- js/scriptEvents.js
+++ js/scriptEvents.js
@@ -1,9 +1,63 @@
 const menuButton = document.querySelector("#menu__itemActive")
 menuButton.classList.add(".menu__item.active");
 
-const menuButtonLink = document.querySelector("#menu__linkActive")
-menuButtonLink.classList.add(".menu__link.active");
 
 
 //leaflet gallery
+
+const seznamAkci = document.querySelector(".events__leaflets-img.main")
+const gulas = document.querySelector(".events__leaflets-img.zero")
+const gulasMenu = document.querySelector(".events__leaflets-img.zero.menu.hidden")
+const rizek = document.querySelector(".events__leaflets-img.one")
+const rizekMenu = document.querySelector(".events__leaflets-img.one.menu.hidden")
+const husa = document.querySelector(".events__leaflets-img.two")
+const vepr = document.querySelector(".events__leaflets-img.three")
+const zverina = document.querySelector(".events__leaflets-img.four")
+const masopust = document.querySelector(".events__leaflets-img.five")
+
+
+
+const noMenu = document.createElement("h2")
+noMenu.classList.add("no-menu")
+noMenu.textContent = "Menu není k dispozici."
+
+const menuToBe = document.createElement("h2")
+menuToBe.classList.add("no-menu")
+menuToBe.textContent = "Menu bude brzy zveřejněno."
+
+
+
+gulas.addEventListener("click", () => {
+  seznamAkci.src = "images/events/gulas_menu.jpeg"
+})
+
+rizek.addEventListener("click", () => {
+    seznamAkci.src = "images/events/rizky_menu.png"
+})
+
+husa.addEventListener("click", () => {
+    seznamAkci.replaceWith(noMenu)
+
+})
+
+vepr.addEventListener("click", () => {
+    seznamAkci.src = "images/events/vepr_hody.png"
+})
+
+
+zverina.addEventListener("click", () => {
+    seznamAkci.replaceWith(menuToBe)
+
+})
+
+masopust.addEventListener("click", () => {
+    seznamAkci.replaceWith(menuToBe)
+
+})
+
+
+
+
+
+
 
