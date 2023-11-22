bee03951a792cec88fc7bde1606d1f68739a1845
Petra Ulicna <petra.ulicna@hotmail.com>
2023-11-22 22:39:51 +0100

    events under 700px js test

--- js/scriptEvents.js
+++ js/scriptEvents.js
@@ -2,66 +2,42 @@ const menuButton = document.querySelector("#menu__itemActive")
 menuButton.classList.add(".menu__item.active");
 
 
+const displayMenu = document.querySelectorAll(".events__news-group-display")
+const menuToDisplay = document.querySelectorAll(".events__news-group--img-menu")
+let selectedMenu = null
 
-//leaflet gallery
+displayMenu.forEach((menu, index) => {
+    menu.addEventListener("click", () => {
+    menu.classList.add("hidden")
+    menuToDisplay[index].classList.toggle("hidden")
+    })
+})
+
+
 
 /*
+displayMenu.forEach((menu, index) => {
+    menu.addEventListener("click", () => {
 
-const seznamAkci = document.querySelector(".events__leaflets-img.main")
-const gulas = document.querySelector(".events__leaflets-img.zero")
-const gulasMenu = document.querySelector(".events__leaflets-img.zero.menu.hidden")
-const rizek = document.querySelector(".events__leaflets-img.one")
-const rizekMenu = document.querySelector(".events__leaflets-img.one.menu.hidden")
-const husa = document.querySelector(".events__leaflets-img.two")
-const vepr = document.querySelector(".events__leaflets-img.three")
-const zverina = document.querySelector(".events__leaflets-img.four")
-const masopust = document.querySelector(".events__leaflets-img.five")
+        if (selectedMenu){
+            selectedMenu.classList.remove("hidden")
+        }
 
+    menu.classList.add("hidden")
+    selectedMenu = menu
 
+    menuToDisplay.forEach((list, menuList) => {
+        if (menuList != index) {
+            list.classList.remove("hidden")
+        }
 
-const noMenu = document.createElement("h2")
-noMenu.classList.add("no-menu")
-noMenu.textContent = "Menu není k dispozici."
+    })
 
-const menuToBe = document.createElement("h2")
-menuToBe.classList.add("no-menu")
-menuToBe.textContent = "Menu bude brzy zveřejněno."
+menuToDisplay[index].classList.toggle("hidden")
 
-
-
-gulas.addEventListener("click", () => {
-  seznamAkci.src = "images/events/gulas_menu.jpeg"
-})
-
-rizek.addEventListener("click", () => {
-    seznamAkci.src = "images/events/rizky_menu.png"
-})
-
-husa.addEventListener("click", () => {
-    seznamAkci.replaceWith(noMenu)
-
-})
-
-vepr.addEventListener("click", () => {
-    console.log("funguju")
-    seznamAkci.src = "images/events/vepr_hody.png"
-    
-})
-
-
-zverina.addEventListener("click", () => {
-    seznamAkci.replaceWith(menuToBe)
-
-})
-
-masopust.addEventListener("click", () => {
-    seznamAkci.replaceWith(menuToBe)
-
+    })
 })
 
 
 */
-
-
-
 
