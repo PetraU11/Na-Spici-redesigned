651a0d31d7e6ef5a331827913eb30c889a03c19f
Petra Ulicna <petra.ulicna@hotmail.com>
2023-11-24 10:52:08 +0100

    events done

--- js/scriptEvents.js
+++ js/scriptEvents.js
@@ -5,6 +5,7 @@ const divWrapper = document.querySelectorAll(".events_news-group-hide-show-wrapp
 const displayMenu = document.querySelectorAll(".events__news-group-display")
 const menuToDisplay = document.querySelectorAll(".events__news-group--img-menu")
 const menuToHide = document.querySelectorAll(".events__news-group-hide")
+const arrowDown = document.querySelectorAll(".button-arrow")
 
 
 divWrapper.forEach((menu, index) => {
@@ -12,7 +13,10 @@ divWrapper.forEach((menu, index) => {
     menu.addEventListener("click", () => {
     
     displayMenu.forEach((toshow, showIndex) => {
-        toshow.classList.add("hidden-button")
+        if(toshow.classList.contains("hidden-button")){
+            toshow.classList.remove("hidden-button")
+           } else { toshow.classList.add("hidden-button")}
+
         if(showIndex != index){
             toshow.classList.remove("hidden-button")
         }
@@ -21,20 +25,32 @@ divWrapper.forEach((menu, index) => {
     menuToDisplay[index].classList.toggle("hidden")
 
     menuToHide.forEach((tohide, hideIndex) =>{
+       if(tohide.classList.contains("hidden-button")){
         tohide.classList.remove("hidden-button")
-         if (hideIndex != index){
-            tohide.classList.add("hidden-button")
-         }
+       } else {tohide.classList.add("hidden-button")}
 
-       
+        if (hideIndex != index){
+            tohide.classList.add("hidden-button")
+         } 
+
+
+         arrowDown.forEach((hideArrow, hideArrowIndex) => {
+            if(hideArrow.classList.contains("hidden-button")){
+                hideArrow.classList.remove("hidden-button")
+               } else {hideArrow.classList.add("hidden-button")}
+
+            if (hideArrowIndex != index){
+                hideArrow.classList.remove("hidden-button")
+             } 
+        })
     })
 
     
-    displayMenu[indexHide].classList.remove("hidden")
-    menuToDisplay[indexHide].classList.add("hidden")
-   
+
+
 
     })
+
 })
 
 
